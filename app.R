211fd597c85d8f7bac075d2c1156fb36af9c4106
Christopher Nowzohour <christopher.nowzohour@teralytics.ch>
2019-06-19 18:30:10 +0200

    First operational version

---
+++ app.R
@@ -0,0 +1,65 @@
+library(shiny)
+library(readr)
+library(plotly)
+
+basePathMac <- "~/Downloads"
+basePathPi <- "/home/pi"
+basePath <- if (dir.exists(basePathPi)) basePathPi else basePathMac
+defaultFile <- "2019-05-29_202306"
+
+ui <- fluidPage(
+  tags$head(
+    tags$style(
+      type="text/css",
+      "body.disconnected {
+         background-color: inherit;
+         opacity: 1;
+       }"
+    )
+  ),
+  plotlyOutput("plot")
+)
+
+server <- function(input, output) {
+  output$plot <- renderPlotly({
+    dat <- read_csv(file.path(basePath, paste0(defaultFile, ".csv")))
+    f <- file(file.path(basePath, paste0(defaultFile, ".log")), "r")
+    log <- readLines(f)
+    close(f)
+    logLinesIrrigationControl <- Filter(function(line) grepl("Irrigation control", line), log)
+    logLinesTimeThreshold <- Filter(function(line) grepl("IRRIGATION_CONTROL_MAX_RATE_H", line), log)
+    logLinesMoistureThreshold <- Filter(function(line) grepl("MIN_MOISTURE_THRESHOLD", line), log)
+    getDateTime <- function(line) as.POSIXct(paste(strsplit(line, " ")[[1]][1:2], collapse = " "), tz = "UTC")
+    dtIrrigationControl <- lapply(logLinesIrrigationControl, getDateTime)
+    dtTimeThreshold <- lapply(logLinesTimeThreshold, getDateTime)
+    dtMoistureThreshold <- lapply(logLinesMoistureThreshold, getDateTime)
+    y2 <- list(
+      overlaying = "y",
+      side = "right",
+      title = "Temperature"
+    )
+    vline <- function(color) function(x) {
+      list(
+        type = "line", 
+        y0 = 0, 
+        y1 = 1, 
+        yref = "paper",
+        x0 = x, 
+        x1 = x, 
+        line = list(color = color)
+      )
+    }
+    plot_ly(dat, type = "scatter", mode = "lines") %>%
+      add_trace(x = ~datetime, y = ~moisture, name = "Moisture") %>%
+      add_trace(x = ~datetime, y = ~temperature, name = "Temperature", yaxis = "y2") %>%
+      layout(
+        title = defaultFile,
+        yaxis2 = y2,
+        yaxis = list(title = "Moisture"),
+        xaxis = list(title = "Time", range = c((as.numeric(Sys.time()) - 7 * 24 * 3600) * 1000L, as.numeric(Sys.time()) * 1000L)),
+        shapes = c(lapply(dtIrrigationControl, vline("gray")), lapply(dtTimeThreshold, vline("blue")), lapply(dtMoistureThreshold, vline("red")))
+      )
+  })
+}
+
+shinyApp(ui, server, options = c(port = 8000))
--- irrigation_control.py
+++ irrigation_control.py
@@ -1,16 +1,95 @@
 from gpiozero import LED
-from time import sleep
 import chirp_modbus
+from datetime import datetime
+import time
+import schedule
+
+
+SAMPLING_RATE_S = 60
+IRRIGATION_CONTROL_MIN_RATE_H = 6
+# IRRIGATION_CONTROL_MIN_RATE_H = 0.1
+IRRIGATION_CONTROL_MAX_RATE_H = 48
+MIN_MOISTURE_THRESHOLD = 270
+VALVE_DURATION_S = 60
+WET_MODE = True
+ML_PER_S = 50.0 / 120.0
+START_WITH_IRRIGATION = False
+
+
+last_irrigation_ts = int(time.time())
+
+
+def get_sensor_readings():
+    moisture = sensor.getMoisture()
+    temperature = sensor.getTemperature()
+    ts = int(time.time())
+    return {"ts": ts, "moisture": moisture, "temperature": temperature}
+
+def ts_to_timestr(ts):
+    return str(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+
+def write_line(line, fname):
+    with open(fname, 'a') as f:
+        f.write(line + '\n')
+
+def write_sensor_readings():
+    readings = get_sensor_readings()
+    print(readings)
+    write_line(
+      ts_to_timestr(readings['ts']) + ',' + str(readings['ts']) + ',' + str(readings['moisture']) + ',' + str(readings['temperature']) + '\n',
+      fname_readings
+    )
+
+def log(msg):
+    line = ts_to_timestr(int(time.time())) + " " + msg
+    print(line)
+    write_line(line, fname_log)
+
+def irrigate(duration_s):
+    global last_irrigation_ts
+    log("Turning valve on")
+    if WET_MODE:
+        valve.on()
+    time.sleep(duration_s)
+    if WET_MODE:
+        valve.off()
+    log("Turning valve off")
+    last_irrigation_ts = int(time.time())
+
+def irrigation_control():
+    log("Irrigation control")
+    readings = get_sensor_readings()
+    log("Sensor readings: " + str(readings))
+    log("last_irrigation_ts: " + str(last_irrigation_ts))
+    if (readings['ts'] - last_irrigation_ts >= IRRIGATION_CONTROL_MAX_RATE_H * 3600):
+        log("IRRIGATION_CONTROL_MAX_RATE_H exceeded")
+        irrigate(VALVE_DURATION_S)
+    if (readings['moisture'] <= MIN_MOISTURE_THRESHOLD):
+        log("MIN_MOISTURE_THRESHOLD exceeded")
+        irrigate(VALVE_DURATION_S)
+
+
+schedule.every(SAMPLING_RATE_S).seconds.do(write_sensor_readings)
+schedule.every(IRRIGATION_CONTROL_MIN_RATE_H).hours.do(irrigation_control)
+
 
 valve = LED(21)
 sensor = chirp_modbus.SoilMoistureSensor(address=1, serialport='/dev/ttyUSB0')
 
+
+date_str = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+fname_readings = date_str + ".csv"
+with open(fname_readings, 'w') as f:
+    f.write('datetime,ts,moisture,temperature\n')
+fname_log = date_str + ".log"
+with open(fname_log, 'w') as f:
+    f.write('Log\n')
+
+
+if START_WITH_IRRIGATION:
+    schedule.run_all()
+
+
 while True:
-		print("Moisture: " + str(sensor.getMoisture()))
-		print("Temperature: " + str(sensor.getTemperature()))
-		print("Turning valve on")
-		valve.on()
-		sleep(5)
-		print("Turning valve off")
-		valve.off()
-		sleep(5)
+    schedule.run_pending()
+    time.sleep(1)
