3505c66036a38d485666817d8df2de40960cabc0
Christopher Nowzohour <cnowzohour@christophersmbp-1.home>
2020-06-23 09:47:52 +0200

    new irrigation control script

--- irrigation_control.py
+++ irrigation_control.py
@@ -21,17 +21,17 @@ logger = logging.getLogger("irrigation_control")
 last_irrigation_ts = int(time.time())
 
 def update_irrigation_db(value):
-    json_body = [
-        {
-            "measurement": "irrigation",
-            "tags": {
-    	    },
-            "fields": {
-                "value": value
-            }
-        }
-    ]
-    client.write_points(json_body)
+	json_body = [
+		{
+			"measurement": "irrigation",
+			"tags": {
+			},
+			"fields": {
+				"value": value
+			}
+		}
+	]
+	client.write_points(json_body)
 
 def update_db_0():
 	update_irrigation_db(0)
@@ -41,7 +41,7 @@ def get_last_moisture():
 	list(result.get_points())[0]['last']
 
 def irrigate(seconds):
-    global last_irrigation_ts
+	global last_irrigation_ts
 	logger.info("Starting irrigation")
 	update_irrigation_db(1)
 	os.system("./on.sh")
@@ -49,29 +49,29 @@ def irrigate(seconds):
 	logger.info("Stopping irrigation")
 	update_irrigation_db(1)
 	os.system("./off.sh")
-    last_irrigation_ts = int(time.time())
+	last_irrigation_ts = int(time.time())
 
 def irrigation_control():
-    log("Irrigation control")
-    moisture = get_last_moisture()
-    current_ts = int(time.time())
-    log("Last soil moisture: " + str(moisture))
-    log("last_irrigation_ts: " + str(last_irrigation_ts))
-    if (current_ts - last_irrigation_ts >= IRRIGATION_CONTROL_MAX_RATE_H * 3600):
-        log("IRRIGATION_CONTROL_MAX_RATE_H exceeded")
-        irrigate(VALVE_DURATION_S)
-    if (moisture <= MIN_MOISTURE_THRESHOLD):
-        log("MIN_MOISTURE_THRESHOLD exceeded")
-        irrigate(VALVE_DURATION_S)
+	log("Irrigation control")
+	moisture = get_last_moisture()
+	current_ts = int(time.time())
+	log("Last soil moisture: " + str(moisture))
+	log("last_irrigation_ts: " + str(last_irrigation_ts))
+	if (current_ts - last_irrigation_ts >= IRRIGATION_CONTROL_MAX_RATE_H * 3600):
+		log("IRRIGATION_CONTROL_MAX_RATE_H exceeded")
+		irrigate(VALVE_DURATION_S)
+	if (moisture <= MIN_MOISTURE_THRESHOLD):
+		log("MIN_MOISTURE_THRESHOLD exceeded")
+		irrigate(VALVE_DURATION_S)
 
 
 schedule.every(IRRIGATION_CONTROL_MIN_RATE_H).hours.do(irrigation_control)
 schedule.every(DB_UPDATE_INTERVAL_S).seconds.do(update_db_0)
 
 if START_WITH_IRRIGATION:
-    schedule.run_all()
+	schedule.run_all()
 
 while True:
-    schedule.run_pending()
-    time.sleep(1)
+	schedule.run_pending()
+	time.sleep(1)
 
